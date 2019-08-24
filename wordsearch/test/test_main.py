969333c0955903df063743fd3a5bdc861e966f23
someone <something@apa.com>
2019-08-24 14:44:39 +0900

    Refactor construction of ArgumentParser in test_main.py
    This removes some duplication across many of the test cases that have
    been written so far by storing the argument parser in a instance
    variable of the test class.

--- wordsearch/test/test_main.py
+++ wordsearch/test/test_main.py
@@ -6,24 +6,21 @@ class ArgumentParserTest(unittest.TestCase):
 
     def setup_method(self, method):
         self.sample_puzzle = 'data/pillar-sample.puzzle'
+        self.argument_parser = wordsearch.build_argument_parser()
 
     def test_build_argument_parser_returns_an_ArgumentParser(self):
-        argument_parser = wordsearch.build_argument_parser()
-        assert argument_parser is not None
-        assert isinstance(argument_parser, argparse.ArgumentParser)
+        assert self.argument_parser is not None
+        assert isinstance(self.argument_parser, argparse.ArgumentParser)
 
     def test_ArgumentParser_contains_a_program_name_called_wordsearch(self):
-        argument_parser = wordsearch.build_argument_parser()
-        assert argument_parser.prog == 'wordsearch'
+        assert self.argument_parser.prog == 'wordsearch'
 
     def test_ArgumentParser_contains_a_description(self):
-        argument_parser = wordsearch.build_argument_parser()
-        assert argument_parser.description is not None
-        assert argument_parser.description != ''
+        assert self.argument_parser.description is not None
+        assert self.argument_parser.description != ''
 
     def test_ArgumentParser_has_a_positional_argument_for_the_input_file(self):
-        argument_parser = wordsearch.build_argument_parser()
-        arguments = argument_parser.parse_args([self.sample_puzzle])
+        arguments = self.argument_parser.parse_args([self.sample_puzzle])
         assert 'puzzle_file' in arguments
         assert arguments.puzzle_file == self.sample_puzzle
 
