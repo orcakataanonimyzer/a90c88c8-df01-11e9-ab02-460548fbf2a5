fcfa9141a9614509b2b8d26f6ba1b273fccb0c89
someone <something@apa.com>
2019-08-24 19:49:01 +0900

    Add docstring to parse_puzzle
    This adds the missing documentation for parse_puzzle.
    
    Closes: #10

--- wordsearch/__init__.py
+++ wordsearch/__init__.py
@@ -15,6 +15,45 @@ import argparse
 __version__ = '0.1.0'
 
 def parse_puzzle(puzzle_file):
+    """Parses a puzzle file, producing a list of words and puzzle board.
+
+    Reads the contents of the open :obj:`file object`, and creates a list of
+    words and a puzzle board. It is assumed that the first line of the file is
+    a comma separated list of words to be searched for in the puzzle board. The
+    puzzle board is assumed to be a square matrix of characters. Each row is on
+    its own line (immediately after the first line) and each character is
+    separated with a comma.
+
+    Example:
+        The following is an example of the expected input. The first line is a
+        comma separated list of words followed by a 15x15 matrix of characters.
+    ::
+
+        BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA
+        U,M,K,H,U,L,K,I,N,V,J,O,C,W,E
+        L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G
+        H,S,U,P,J,P,R,J,D,H,S,B,X,T,G
+        B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E
+        A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D
+        S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F
+        B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z
+        O,K,R,I,K,A,M,M,R,M,F,B,A,P,P
+        N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S
+        E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K
+        S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D
+        T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K
+        O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H
+        W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S
+        K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B
+
+    Args:
+        puzzle_file (:obj:`file object`): An open file containing the puzzle to
+            be parsed.
+
+    Returns:
+        A two-tuple containing the :obj:`list` of words and the puzzle board as
+        a square, two-dimensional :obj:`list`.
+    """
     if puzzle_file is None:
         raise ValueError('Invalid argument: puzzle_file must not be None.')
     words = None
